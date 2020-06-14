6e16db710c7a835e1280ce01e69b2678d244aeeb
lihuiming <viomay@163.com>
2020-06-15 00:41:35 +0800

    初识Frame

---
+++ src/main/java/com/leon/tank/FristFrame.java
@@ -0,0 +1,23 @@
+package com.leon.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class FristFrame {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setSize(800, 600);
+        frame.setResizable(false);
+        frame.setTitle("tank war");
+        frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
